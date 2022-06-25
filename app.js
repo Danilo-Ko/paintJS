dc84fa76765c0ce5573dbf0e9d1013a43bcc9dc4
Danilo-Ko <danilosrbija@gmail.com>
2022-06-25 12:34:31 +0200

    chaning colorsg

--- app.js
+++ app.js
@@ -1,38 +1,42 @@
 const canvas = document.getElementById("jsCanvas");
-const ctx = canvas.getContext ("2d");
+const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("bgColor");
 
-canvas.width =700;
-canvas.height =700;
+canvas.width = 700;
+canvas.height = 700;
 
 ctx.strokeStyle = "black";
 ctx.lineWidth = 2.5;
 
+let painting = false;
 
-let painting = true;
-
-function stopPainting () {
-  painting = false;
+function stopPainting() {
+    painting = false;
 }
-function startPainting () {
-  painting = true;
-}
-
-function onMouseMove (event) {
-const x = event.offsetX;
-const y = event.offsetY;
-if(!painting) {
-  ctx.beginPath ();
-  ctx.moveTo(x,y);
-}else {
-  ctx.lineTo(x,y);
-  ctx.stroke();
-}
+function startPainting() {
+    painting = true;
 }
 
+function onMouseMove(event) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+}
 
-
+function handleColorClick (event) {
+  const color =event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+}
 
 canvas.addEventListener("mousemove", onMouseMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", stopPainting);
 canvas.addEventListener("mouseleave", stopPainting);
+
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
